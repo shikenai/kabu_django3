010dcfdd1de9006394d05a4e2d95de67cbc4b8c4
yoshikazukakehashi <kakehashi.y2788@gmail.com>
2023-01-18 11:57:46 +0900

    my_modulesの実行に成功

--- stocks/views.py
+++ stocks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from stocks.my_modules import my_function
 
 
 # Create your views here.
 def index(request):
+    my_function.hello()
     return render(request, 'index.html')

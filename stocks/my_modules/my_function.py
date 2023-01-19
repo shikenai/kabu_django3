7c451c4260bad058f8495f4fb39a0f19258df5d1
yoshikazukakehashi <kakehashi.y2788@gmail.com>
2023-01-19 12:04:16 +0900

    今から、my_function.pyでbulk_createしようとしているところ

--- stocks/my_modules/my_function.py
+++ stocks/my_modules/my_function.py
@@ -1,5 +1,15 @@
-from stocks.my_modules import sub_function
+import pandas as pd
+from kabu_django3.settings import BASE_DIR
+from stocks.models import Test
+df = pd.read_csv(BASE_DIR / "data/test.csv")
 
+df_records = df.to_dict()
+print(df_records)
 
-def hello():
-    print('hello')
+test_model_inserts = []
+for d in df_records:
+    test_model_inserts.append(Test(
+        num=d[0],
+        name=d[1]
+    ))
+Test.objects.bulk_create(test_model_inserts)

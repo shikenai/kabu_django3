d963db75394f1c487977ddbaf3c006aa4239fd17
yoshikazukakehashi <kakehashi.y2788@gmail.com>
2023-01-21 14:26:04 +0900

    models.pyにPricesを追加

--- stocks/models.py
+++ stocks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 from django.db import models
@@ -10,15 +11,27 @@ class Brand(models.Model):
     brand_name = models.CharField(max_length=50, blank=True, null=True)
     code = models.IntegerField(blank=True, null=True)
     division = models.CharField(max_length=20, blank=True, null=True)
-    industry_code_1 = models.IntegerField(blank=True, null=True)
+    industry_code_1 = models.CharField(max_length=10, blank=True, null=True)
     industry_division_1 = models.CharField(max_length=10, blank=True, null=True)
-    industry_code_2 = models.IntegerField(blank=True, null=True)
+    industry_code_2 = models.CharField(max_length=10, blank=True, null=True)
     industry_division_2 = models.CharField(max_length=10, blank=True, null=True)
-    scale_code = models.IntegerField(blank=True, null=True)
+    scale_code = models.CharField(max_length=10, blank=True, null=True)
     scale_division = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.brand_name + "(" + self.market + ":" + str(self.code) + ")"
+
+
+class Prices(models.Model):
+    brand = models.ForeignKey(to=Brand, on_delete=models.CASCADE)
+    trade_date = models.DateField(blank=True, null=True, auto_now=True)
+    start_value = models.FloatField(verbose_name='始値', blank=True, null=True)
+    end_value = models.FloatField(verbose_name='終値', blank=True, null=True)
+    max_value = models.FloatField(verbose_name='高値', blank=True, null=True)
+    min_value = models.FloatField(verbose_name='安値', blank=True, null=True)
+
+    def __str__(self):
+        return "株価" + self.brand.brand_name + self.trade_date.strftime("%Y年%m月%d日")
 
 
 class Test(models.Model):
